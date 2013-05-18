7c9244702711e020a54e13d3418ebcd5b873fbd5
Vincent Trivett <trivett@gmail.com>
2013-05-18 13:30:33 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bibliotravel::Application.config.secret_token = 'bb9385345bb5f8e51c5d273deb67040da5d84a62439ba03ffaba6bae57561da3dc2c346386f2e351d36d38209aced3613c36443ce9baa5f1e321080bdb290107'
