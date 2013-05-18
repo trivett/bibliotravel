e3c678b568ca08a1f215a4033b621222574c490a
Charlie Gaines <beaugaines@gmail.com>
2013-05-18 14:20:40 -0400

    session controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Bibliotravel::Application.routes.draw do
  
+  get "sessions/create"
+
   match 'auth/:provider/callback', to: 'sessions#create'
   match 'auth/failure', to: redirect('/')
   match 'signout', to: 'sessions#destroy', as: 'signout'
