7c894bcb2bb1c7da7c274100af2385815a0fe756
Charlie Gaines <beaugaines@gmail.com>
2013-05-19 11:07:35 -0400

    index and geocode method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,9 @@
 class WelcomeController < ApplicationController
   def index
-    @city = City.first
+    @city = request.location.city
+    if @city.nil?
+      @city = City.first
+    end
     @books = Book.where(:city_id => @city.id).limit(15)
     @authors = @city.authors.limit(5)
   end
