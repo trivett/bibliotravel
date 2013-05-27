2f1842ce03d234db6f8f53e4099571a530901011
Charlie Gaines <beaugaines@gmail.com>
2013-05-27 17:44:51 -0400

    lon books list, seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,10 +13,14 @@ Author.delete_all
 
 c1 = City.create(name: 'New York', country: 'USA')
 c2 = City.create(name: 'San Francisco', country: 'USA')
+c3 = City.create(name: 'London', country: 'UK')
+c4 = City.create(name: 'Mexico City', country: 'Mexico')
+
 Author.create(name: 'Herman Melville', city_id: c1.id, blurb: 'American novelist, short story writer, essayist, and pffoet. He is best known for his novel Moby-Dick. His first three books gained much contemporary attention (the first, Typee, became a bestseller), but after a fast-blooming literary success in the late 1840s, his popularity declined precipitously in the mid-1850s and never recovered during his lifetime.')
 
 file1 = Rails.root + "nybooks.csv"
 file2 = Rails.root + "sfbooks.csv"
+file3 = Rails.root + "londonbooks.csv"
 
 CSV.foreach(file1, :headers => true) do |row|
   a = Author.create!({ :name => row[1]} )
@@ -27,11 +31,11 @@ CSV.foreach(file1, :headers => true) do |row|
   })
 end
 
-CSV.foreach(file2, :headers => true) do |row|
-  a = Author.create!({ :name => row[1]} ) 
-  Book.create!({
-    :title => row[0],
-    :author_id => a.id,
-    :city_id => c2.id
-  })
-end
+# CSV.foreach(file2, :headers => true) do |row|
+#   a = Author.create!({ :name => row[1]} ) 
+#   Book.create!({
+#     :title => row[0],
+#     :author_id => a.id,
+#     :city_id => c2.id
+#   })
+# end
---
+++ lib/tasks/scrape_books.rake
@@ -0,0 +1,10 @@
+require 'rubygems'
+require 'open-uri'
+require 'nokogiri'
+
+puts 'Scraping guardian site'
+
+url1 = "http://www.guardian.co.uk/travel/2011/may/20/top-10-books-on-london"
+
+page = Nokogiri::HTML(open(url1))
+
