5ee581fb332638885d4ea27759ee1fbaa85c2ff4
Charlie Gaines <beaugaines@gmail.com>
2013-05-30 14:00:56 -0400

    book tags helper

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -5,4 +5,15 @@ module BooksHelper
     img.size == 0 ? image_tag('default_cover.png') : image_tag(img)
   end
 
+  def book_tags_helper item
+    tags_set = Set.new
+    tags_set.merge(item.tags)
+    res = ''
+    tags_set.each do |t|
+      res << content_tag(:li, :class => 'btn filter', :data => { :filter => t }) do
+        t.titleize
+      end
+    end
+    res
+  end
 end
