bafa43489689a7d43495a360d8c126f447edac98
Charlie Gaines <beaugaines@gmail.com>
2013-05-30 00:08:26 -0400

    asset precompile config

--- config/application.rb
+++ config/application.rb
@@ -56,6 +56,9 @@ module Bibliotravel
     # Enable the asset pipeline
     config.assets.enabled = true
 
+    # disable assets init precompile
+    config.assets.initialize_on_precompile = false
+
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
   end
