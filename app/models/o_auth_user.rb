9e42fa2b221e475bd25fd1ec16d21aa5441f7613
Charlie Gaines <beaugaines@gmail.com>
2013-06-05 17:14:58 -0400

    oauth model methods

--- app/models/o_auth_user.rb
+++ app/models/o_auth_user.rb
@@ -6,6 +6,7 @@ class OAuthUser
     @auth = creds
     @user = user
     @provider = @auth.provider
+    @policy = "#{@provider}_policy".classify.constantize.new(@auth)
   end
 
   def login_or_create
@@ -15,4 +16,62 @@ class OAuthUser
   def logged_in?
     @user.present?
   end
+
+  private
+
+  def login
+    @account = Account.where(@auth.slice("provider", "uid")).first
+    if @account.present?
+      refresh_tokens
+      @user = @account.user
+      @policy.refresh_callbacks(@account)
+    else
+      false
+    end
+  end
+
+  def refresh_tokens
+    @account.update_attributes(
+      :oauth_token => @policy.oauth_token,
+      :oauth_secret => @policy.oauth_secret,
+      :oauth_expires => @policy.oauth_expires
+      )
+  end
+
+  def create_new_account
+    create_new_user if @user.nil?
+
+    unless account_already_exists?
+      @account = @user.accounts.create(
+        :provider => @provider,
+        :uid => @policy.uid,
+        :oauth_token => @policy.oauth_token,
+        :oauth_expires => @policy.oauth_expires,
+        :oauth_secret => @policy.oauth_secret,
+        :username => @policy.username
+        )
+      @policy.create_callback(@account)
+    end
+  end
+
+  def account_already_exists?
+    @user.accounts.exists?(@provider.uid, @policy.uid)
+  end
+
+  def create_new_user
+    @user = User.create(
+      :first_name => @policy.first_name,
+      :last_name => @policy.last_name,
+      :email => @policy.email,
+      :picture => image
+      )
+  end
+
+  def image
+    image = open(URI.parse(@policy.image_url), :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE)
+    def image.original_filename; base_uri.path.split('/').last; end
+    image
+  end
+
+  
 end
