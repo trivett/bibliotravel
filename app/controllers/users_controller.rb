288cb62bdfdd3612a78132eda4d985c2d721ccd5
Charlie Gaines <beaugaines@gmail.com>
2013-06-05 14:22:23 -0400

    users cont methods

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,24 @@
 class UsersController < ApplicationController
+
+  before_action :authenticate_user, only: [:edit, :update]
+
   def new
+    @user = RegularUser.new
   end
 
   def create
+    @user = RegularUser.new(params[:regular_user])
+    if @user.save
+      session[:user_id] = @user.id
+      flash[:notice] = 'User successfully created.'
+      redirect_to root_path
+    else
+      flash.now[:alert] = 'User creation failed, try again'
+      render action: 'new'
+    end
   end
 
   def show
+    @user = current_user
   end
 end
