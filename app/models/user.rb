73c4fde66fd8a8c74ec9cd61fc0c63f382258b72
Charlie Gaines <beaugaines@gmail.com>
2013-06-05 23:03:42 -0400

    user#facebook

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
   has_many :accounts, :dependent => :destroy
   attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :picture_url
 
+  def facebook
+    o_token = self.accounts.where(provider: 'facebook').first.oauth_token
+    @facebook ||= Koala::Facebook::API.new(o_token)
+  end
+
   def has_facebook?
     accounts.where(provider: 'facebook').any?
   end
