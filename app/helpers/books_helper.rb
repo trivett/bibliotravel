73798919496976b1f70eb065b20a7011736def35
Charlie Gaines <beaugaines@gmail.com>
2013-06-07 17:58:08 -0400

    improved book_cover_helper

--- app/helpers/books_helper.rb
+++ app/helpers/books_helper.rb
@@ -1,8 +1,21 @@
 module BooksHelper
 
+  require 'amazon/ecs'
+
+  Amazon::Ecs.options = {
+    :associate_tag => 'bookcities-20',
+    :AWS_access_key_id => ENV['AMAZON_KEY'],
+    :AWS_secret_key => ENV['AMAZON_SECRET']
+  }
+
+
+
   def book_cover_helper item
-    img = "http://covers.librarything.com/devkey/#{ENV["LIB_THING_KEY"]}/large/isbn/#{item.isbn}"
-    img.size == 0 ? image_tag('default_cover.png') : image_tag(img)
+    res = Amazon::Ecs.item_search(item.isbn, { :response_group => 'Images'} )
+    img_hash = res.items.first.get_hash('LargeImage')
+    image_tag(img_hash['URL'], :size => '193x300')
+    # img = "http://covers.librarything.com/devkey/#{ENV["LIB_THING_KEY"]}/large/isbn/#{item.isbn}"
+    # img.size == 0 ? image_tag('default_cover.png') : image_tag(img)
   end
 
   def book_tags_helper item
