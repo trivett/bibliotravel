38ab321a124e629f9571452a09b855ed01bd8914
Charlie Gaines <beaugaines@gmail.com>
2013-06-07 22:13:30 -0400

    user slugs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ActiveRecord::Base
+  include InferredSlug::Slug
+
   has_many :accounts, :dependent => :destroy
   attr_accessible :email, :first_name, :last_name, :password, :password_confirmation, :picture_url
+
+  def slug
+    "#{id}-#{first_name}-#{last_name}"
+  end
 
   def facebook
     o_token = self.accounts.where(provider: 'facebook').first.oauth_token
