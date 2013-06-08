abb8d88fb8a668342e3da22e50984465ba999ed3
Charlie Gaines <beaugaines@gmail.com>
2013-06-07 22:32:03 -0400

    nice user and book slugs

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,18 @@
 class Book < ActiveRecord::Base
+  include InferredSlug::Slug
+
   attr_accessible :date, :title, :city_id, :author_id, :tags, :blurb, :isbn
   belongs_to :author
   belongs_to :city
+
+  def slug
+    tit = self.title.parameterize.strip
+    tit.gsub! /['`]/, ''
+    tit.gsub! /\s*[^A-Za-z0-9\.]\s*/, '-'
+    tit.gsub! /-+/, '-'
+    # tit.gsub! /\A[-\.]+|[-\.]+\z/, ''
+    "#{id}-#{tit}"
+  end
 
   def self.search(search)
     begin
